f411be9727d185eca13633654f345e03b9edc007
scartag <tag@crowdbotics.com>
2022-12-07 18:13:31 +0000

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,45 @@
+const styles = StyleSheet.create({
+  "hr": {
+    "marginTop": 20,
+    "marginBottom": 20,
+    "borderBottomColor": "black",
+    "borderBottomWidth": 1
+  },
+  "container": {
+    "flex": 1,
+    "height": 100,
+    "padding": 13
+  },
+  "text": {
+    "color": "black",
+    "fontSize": 20
+  },
+  "buttonPressed": {
+    "backgroundColor": "aquamarine"
+  },
+  "buttonNotPressed": {
+    "backgroundColor": "blue"
+  },
+  "button": {
+    "borderRadius": 4,
+    "padding": 15,
+    "marginTop": 10
+  },
+  "buttonText": {
+    "color": "white",
+    "textAlign": "center",
+    "fontSize": 16
+  }
+});
 export const globalOptions = {
-  name: "scarappcode9-dev-49307",
-  url: "https://scarappcode9-dev-49307.botics.co",
-  api: "https://scarappcode9-dev-49307.botics.co/api/v1"
+  "name": "scarappcode9-dev-49307",
+  "url": "https://scarappcode9-dev-49307.botics.co",
+  "api": "https://scarappcode9-dev-49307.botics.co/api/v1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
-    copy: "Routes available!"
+    "title": "App Menu7",
+    "copy": "Routes available!",
+    "styles": styles
   }
 };
